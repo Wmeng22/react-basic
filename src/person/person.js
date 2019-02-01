c4d58adbb090b730fa8e2b623f5a06b6a09a7981
smeng <406591480@qq.com>
2019-02-01 09:18:32 +0800

    a case with react

---
+++ src/person/person.js
@@ -0,0 +1,19 @@
+
+import React  from 'react';
+import './person.css'
+const person = (props) => {
+    return (
+        <div >
+            <p  onClick={props.myclick}>大家好我是{props.name},我有{props.count}</p>
+            <p className='person'>{props.children}</p>
+            <input type="text" onChange={props.mychange} defaultValue={props.name}/>
+        </div>
+    )
+}
+
+export default person
+
+
+
+
+
